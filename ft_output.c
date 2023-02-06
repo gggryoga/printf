28fe785d1195d4ce0ef474dfc6ca5beab141d6a9
Ryoma Ozeki <rozeki@c1r14s1.42tokyo.jp>
2023-02-06 18:05:18 +0900

    hi

--- ft_output.c
+++ ft_output.c
@@ -1,3 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_output.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rozeki <rozeki@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/06 15:26:29 by rozeki            #+#    #+#             */
+/*   Updated: 2023/02/06 17:09:38 by rozeki           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "printf.h"
 
-void ft_printchar( 
+int ft_printchar(char c)
+{
+	int	size;
+	
+	size = write (1, &c, 1);
+	return (size);
+}
+
+int ft_printStr(char *str)
+{
+	int size;
+	size_t	count;
+
+	size = 0;
+	count = 0;
+	if (!str)
+		size = write(1, "(null)", 6);
+	else
+	{
+		while (str[count])
+		{
+			if (ft_printchar(*str) == -1)
+				return (-1);
+			size ++;
+		}
+	}
+	return (size);
+}
+
+int ft_printPointer(uintptr_t dst)
+{
+	
+}
--- ft_printf.c
+++ ft_printf.c
@@ -1,28 +1,41 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   printf.c                                           :+:      :+:    :+:   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rozeki <rozeki@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/26 15:06:15 by rozeki            #+#    #+#             */
-/*   Updated: 2023/02/06 15:25:21 by rozeki           ###   ########.fr       */
+/*   Updated: 2023/02/06 16:46:12 by rozeki           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "printf.h"
 
+int ft_lenfree()
+
 int	ft_ChFormat(va_list args, const char size)
 {
-	size_t	len;
+	int	len;
 
 	len = 0;
 	if (size == 'c')
-		len += ft_printchar(args);
+		len == ft_printchar(args);
 	else if (size == 'd')
-		len += ft_printInt(args);
+		len == ft_printInt(args);
 	else if (size == 's')
-		len += ft_printstr(args);
+		len == ft_printstr(args);
+	else if (size == 'P')
+		len == ft_print
+	else if (size == 'i')
+		len == ft_printinteger(args);
+	else if (size == 'u')
+		len == ft_print 
+	else if (size == 'x')
+		len == ft_print
+	else if (size == 'X')
+	else if (size == '%')
+	return (len);
 }
 
 void print_setting(const char* format, ...)
@@ -41,10 +54,15 @@ void print_setting(const char* format, ...)
 			len += ft_ChFormat(args, format[index + 1]);
 			index++;
 		}
+		else if (format[index] == -1)
+			
 	}
 }
 
 int main()
 {
-	print_setting("42tokyo");
+	// print_setting("42tokyo");
+	char * a = "Hello";
+	int b = 42;
+	printf("%d",b);
 }
