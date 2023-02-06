4781d43a4915a7d1184a6145a7139a9e2d058c67
Ryoma Ozeki <rozeki@c1r1s1.42tokyo.jp>
2023-02-06 15:25:38 +0900

    hi

--- ft_printf.c
+++ ft_printf.c
@@ -6,26 +6,42 @@
 /*   By: rozeki <rozeki@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/26 15:06:15 by rozeki            #+#    #+#             */
-/*   Updated: 2023/01/26 16:29:19 by rozeki           ###   ########.fr       */
+/*   Updated: 2023/02/06 15:25:21 by rozeki           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "printf.h"
 
-// int	ft_printf(const char *, ...)
-// {
-	
-// }
+int	ft_ChFormat(va_list args, const char size)
+{
+	size_t	len;
+
+	len = 0;
+	if (size == 'c')
+		len += ft_printchar(args);
+	else if (size == 'd')
+		len += ft_printInt(args);
+	else if (size == 's')
+		len += ft_printstr(args);
+}
 
 void print_setting(const char* format, ...)
 {
-    va_list args;
+    va_list		args;
+	size_t		len;
+	size_t		index;
+
+	index = 0;
+	len = 0;
     va_start( args, format );
-
-	va_arg (args, format);	
-    printf( "\n" );
-
-    va_end( args );
+	while (format[index])
+	{
+		if (format[index] == '%')
+		{
+			len += ft_ChFormat(args, format[index + 1]);
+			index++;
+		}
+	}
 }
 
 int main()
